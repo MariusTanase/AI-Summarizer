597a623c7f8a0ab42739ff2b097cde93f17a50e3
MariusTanase <pulsemusic8@hotmail.com>
2023-07-09 15:55:07 +0100

    Removing flex-row as by default they are into a row

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { logo } from '../assets'
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center flex-row w-full p-3 mb-7'>
+      <nav className='flex justify-between items-center w-full p-3 mb-7'>
         <h2 className='text-white bg-black p-2 rounded-lg text-2xl'>AI Summz</h2>
         <button type='button' onClick={() => window.open('https://github.com/MariusTanase/AI-Summarizer')} className='ease-in-out duration-300 rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black'>
           Github
