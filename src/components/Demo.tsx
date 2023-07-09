a44a650f83d31c29f93e8287353e9ec6a3b8ebe9
MariusTanase <pulsemusic8@hotmail.com>
2023-07-09 16:32:39 +0100

    Installing React-Redux, few changes to demo component

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,12 +1,25 @@
 import {useState, useEffect} from 'react'
 import {copy, linkIcon, loader, tick} from '../assets';
+
 const Demo = () => {
+    const [article, setArticle] = useState({
+      url: '',
+      summary: ''
+    });
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      
+    }
+
+
   return (
     <section className='mt-16 w-full max-w-xl'>
 
       <div className='flex flex-col w-full gap-2'>
         <form className='flex relative justify-center items-center'
-          onSubmit={()=> {}}>
+          onSubmit={(e) => handleSubmit(e)}>
             <img
             src={linkIcon}
             alt="link icon"
@@ -15,8 +28,8 @@ const Demo = () => {
           <input className='peer block w-full rounded-lg border-2 transition-all duration-300 ease-in-out outline-none border-gray-300 bg-white shadow-lg font-satoshi focus:border-black focus:outline-none focus:ring-0 py-2.5 pl-10 pr-12 text-sm'
             type='url'
             placeholder='Enter the article URL'
-            onChange={()=> {}}
-            value=""
+            onChange={(e)=> {setArticle({...article, url: e.target.value})}}
+            value={article.url}
             required
           />
 
@@ -27,7 +40,12 @@ const Demo = () => {
            <span>Search</span>
           </button>
           </form>
+
+          {/* Searched URLs history */}
+
+
       </div>
+        {/* Display the result form API */}
 
     </section>
   )
