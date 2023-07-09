8be6b3b67bdcf4f3074cc071438a93fa1453adaf
MariusTanase <pulsemusic8@hotmail.com>
2023-07-09 16:20:15 +0100

    Style of the button inside the form

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -12,13 +12,20 @@ const Demo = () => {
             alt="link icon"
             className='absolute left-0 my-2 ml-2 w-5'
           />
-          <input className='block w-full rounded-lg border-2 transition-all duration-300 ease-in-out outline-none border-gray-300 bg-white shadow-lg font-satoshi focus:border-black focus:outline-none focus:ring-0 py-2.5 pl-10 pr-12 text-sm'
+          <input className='peer block w-full rounded-lg border-2 transition-all duration-300 ease-in-out outline-none border-gray-300 bg-white shadow-lg font-satoshi focus:border-black focus:outline-none focus:ring-0 py-2.5 pl-10 pr-12 text-sm'
             type='url'
             placeholder='Enter the article URL'
             onChange={()=> {}}
             value=""
             required
           />
+
+          <button
+          type='submit'
+          className='peer-focus:border-gray-700 peer-focus:text-gray-700 hover:border-gray-700 transition-all duration-300 ease-in-out hover:text-gray-700 absolute inset-y-0 text-xl flex justify-center items-center right-0 my-1.5 mr-1.5 flex w-fit px-2 items-center justify-center rounded border border-gray-200 font-sans text-sm font-medium text-gray-400'
+          >
+           <span>Search</span>
+          </button>
           </form>
       </div>
 
